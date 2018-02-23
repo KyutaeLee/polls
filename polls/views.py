b65ce30c2ab1c89d675c4c05d6ce1f19dd6b37ff
kyutaelee <rbxofjqn@gmail.com>
2018-02-23 20:58:01 +0900

    1day 1commit

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,41 @@
-from django.http import Http404
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views.generic import ListView, DetailView
+from django.views import generic
+from .models import Choice, Question
 
-from .models import Question
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+def IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+    def get_queryset(self):
+        return Question.objects.orderby("-pub_date")[:5]
 
-def results(request, question_id):
-    return HttpResponse("you are looking at results of question %s" % question_id)
+def DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+def ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
-    return HttpResponse("you are voting on question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try :
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question':question,
+            'error message':"you didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
 
